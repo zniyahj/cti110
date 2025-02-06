cb9708aa7aa025450210500aaa47e21dc6df9342
zniyahj <johnsonz1512@student.faytechcc.edu>
2025-02-06 14:22:26 -0500

    Add files via upload

---
+++ P1LAB1_JohnsonZNiyah.py
@@ -0,0 +1,12 @@
+# ZNiyah Johnson
+# 2-4-2025
+# P1LAB1
+# This program will demo python code
+
+
+fname = input("Enter your first name: ")
+
+lname = input("Enter your last name: ")
+
+print("Hello, ",fname, str(lname) + "! Welcome to CT!-110")
+
