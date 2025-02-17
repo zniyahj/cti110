adfa1d44524c04f5dd945ae4c54b5a96ce066983
zniyahj <johnsonz1512@student.faytechcc.edu>
2025-02-16 22:27:56 -0500

    Add files via upload

---
+++ P1HW1_JohnsonZniyah.py
@@ -0,0 +1,46 @@
+# ZNiyah Johnson
+# 2/16/2025
+# P1HW1
+# import math
+# \n is the newline character
+
+print("\n-----Calculating Exponents-----\n\n")
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print("\n")
+value = base**exponent
+value = pow(base, exponent)
+#value = math.pow(base, exponent)
+print(base,"raised to the power of",exponent,"is",value,"!!")
+
+
+
+
+##print("-"*20,"Calculating Exponents","-"*20)
+
+
+
+print("\n-----Addition and Subtraction-----\n\n")
+
+
+
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+print("\n")
+value = num1 + num2 - num3
+print(num1,"+",num2,"-",num3,"is equal to",value)
+
+
+
+
+
+
+##num1 = int(input("Enter a number: "))
+##num2 = int(input("Enter another number: "))
+##
+##num3 = num1 + num2
+##num3 = str(num3)
+##print(num1,"=",num2,"=",num3)
+##
+##input("Press any key to continue!!")
